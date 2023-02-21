30213fa9834616cf1cd750ff2576c7cb8bfc08df
izonejangwonyoung <2000shim@naver.com>
2023-02-21 21:06:01 +0900

    add moogoose

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
-//
+import mongoose from "mongoose";
+
 //
 // const mongoose = require('mongoose')
 //
